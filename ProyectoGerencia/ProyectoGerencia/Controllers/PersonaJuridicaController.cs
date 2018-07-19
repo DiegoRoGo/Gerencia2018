efb88c1508a56cd6af34216ce092595444374093
Ricardo Gamboa <ricardogamboa@live.com>
2018-07-19 04:38:49 -0600

    Registro de Persona juridica sin email ni documento

--- ProyectoGerencia/ProyectoGerencia/Controllers/PersonaJuridicaController.cs
+++ ProyectoGerencia/ProyectoGerencia/Controllers/PersonaJuridicaController.cs
@@ -1,4 +1,5 @@
 ﻿using ProyectoGerencia.DataBase.Configuration;
+using ProyectoGerencia.DataBase.Entities;
 using ProyectoGerencia.ViewModels.RegistroNormalVMs;
 using System;
 using System.Collections.Generic;
@@ -73,7 +74,7 @@ namespace ProyectoGerencia.Controllers
 
         public ActionResult RegistrarSegundoRepresentante(RegistroRepresentanteLegalVM RepresentanteLegal)
         {
-            RegistroRepresentanteLegal2VM R2 = new RegistroRepresentanteLegal2VM
+            return View(new RegistroSegundoRepresentanteLegalVM
             {
                 Tipos = new[]
                 {
@@ -87,17 +88,119 @@ namespace ProyectoGerencia.Controllers
                 IdentificacionRep1 = RepresentanteLegal.Identificacion,
                 NombreRep1 = RepresentanteLegal.Nombre,
                 TipoIdentificacionRep1 = RepresentanteLegal.TipoSeleccionado
-            };
-            return View(R2);
+            });
         }
 
         [HttpPost]
-        public ActionResult RegistrarSegundoRepresentante(RegistroRepresentanteLegal2VM RepresentanteLegal)
+        public ActionResult RegistrarSegundoRepresentante(RegistroSegundoRepresentanteLegalVM RepresentanteLegal)
         {
             if (ModelState.IsValid)
             {
-                return View();
+                if(RepresentanteLegal.Cedula != RepresentanteLegal.IdentificacionRep1)
+                {
+                    return RedirectToAction("RegistroDeOperadores", "PersonaJuridica", RepresentanteLegal);
+                }
             }
+            RepresentanteLegal.Tipos = new[]
+            {
+                new SelectListItem { Value = "1", Text = "Nacional" },
+                new SelectListItem { Value = "2", Text = "DIMEX" },
+                new SelectListItem { Value = "3", Text = "Pasaporte" }
+            };
+            return View(RepresentanteLegal);
+        }
+
+        public ActionResult RegistroDeOperadores(RegistroSegundoRepresentanteLegalVM Representante)
+        {
+            return View(new RegistroDeOperadoresVM()
+            {
+                ContrasenaPersonaJuridica = Representante.ContrasenaPersonaJuridica,
+                CorreoElectronicoPersonaJuridica = Representante.CorreoElectronicoPersonaJuridica,
+                CorreoRep1 = Representante.CorreoRep1,
+                CorreoRep2 = Representante.CorreoElect,
+                IdentificacionRep1 = Representante.IdentificacionRep1,
+                IdentificacionRep2 = Representante.Cedula,
+                NombreRep1 = Representante.NombreRep1,
+                NombreRep2 = Representante.NombreCompleto,
+                TipoIdentificacionRep1 = Representante.TipoIdentificacionRep1,
+                TipoIdentificacionRep2 = Representante.TipoSeleccionado
+            });
+        }
+
+        [HttpPost]
+        public ActionResult RegistroDeOperadores(RegistroDeOperadoresVM Operador)
+        {
+            if (String.IsNullOrEmpty(Operador.Email))
+            {
+                if(Operador.Operadores.Count > 0)
+                {
+                    using(var Context = new Context())
+                    {
+                        var PersonaJuridica = Context.PersonasJuridicas.Add(new DataBase.Entities.PersonaJuridica
+                        {
+                            Contrasena = Operador.ContrasenaPersonaJuridica,
+                            Correo = Operador.CorreoElectronicoPersonaJuridica,
+                            Operadores = new List<Cuenta>(),
+                            RepresentanteLegales = new List<RepresentanteLegal>()
+                        });
+
+                        foreach (var Item in Operador.Operadores)
+                        {
+                            PersonaJuridica.Operadores.Add(Context.Cuentas.Where(x=> x.Correo == Item).SingleOrDefault());
+                        }
+
+                        PersonaJuridica.RepresentanteLegales.Add(new DataBase.Entities.RepresentanteLegal
+                        {
+                            CorreoElectronico = Operador.CorreoRep1,
+                            Identificacion = Operador.IdentificacionRep1,
+                            Nombre = Operador.NombreRep1,
+                            Tipo = (RepresentanteLegal.TipoIdentificacion)Enum.Parse(typeof(RepresentanteLegal.TipoIdentificacion), Operador.TipoIdentificacionRep1)
+                        });
+
+                        PersonaJuridica.RepresentanteLegales.Add(new DataBase.Entities.RepresentanteLegal
+                        {
+                            CorreoElectronico = Operador.CorreoRep2,
+                            Identificacion = Operador.IdentificacionRep2,
+                            Nombre = Operador.NombreRep2,
+                            Tipo = (RepresentanteLegal.TipoIdentificacion)Enum.Parse(typeof(RepresentanteLegal.TipoIdentificacion), Operador.TipoIdentificacionRep2)
+                        });
+
+                        Context.SaveChanges();
+                    }
+                    return RedirectToAction("ConfirmacionRegistro", "PersonaJuridica", Operador.CorreoElectronicoPersonaJuridica);
+                }
+                ViewBag.Error = "Es necesario registrar operadores";
+                return View(Operador);
+            }
+            
+            using(var Context = new Context())
+            {
+                var Cuenta = Context.Cuentas.Where(x => x.Correo == Operador.Email).SingleOrDefault();
+                if (Cuenta != null)
+                {
+                    if(Cuenta.PersonaJuridica != null)
+                    {
+                        ViewBag.Error = "El usuario ya es operador de una persona juridica, no se ha podido agregar";
+                        return View(Operador);
+                    }
+                    if (Operador.Operadores.Any(x=> x == Operador.Email))
+                    {
+                        ViewBag.Error = "El operador ya habia sido agregado";
+                        return View(Operador);
+                    }
+                    Operador.Operadores.Add(Operador.Email);
+                    Operador.Email = "";
+                    return View(Operador);
+                }
+            }
+            ViewBag.Error = "El correo es inexistente";
+            return View(Operador);
+        }
+
+
+        public ActionResult ConfirmacionRegistro(string Email)
+        {
+            ViewBag.correo = Email;
             return View();
         }
     }
--- ProyectoGerencia/ProyectoGerencia/ViewModels/RegistroNormalVMs/RegistroSegundoRepresentanteLegalVM.cs
+++ ProyectoGerencia/ProyectoGerencia/ViewModels/RegistroNormalVMs/RegistroSegundoRepresentanteLegalVM.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace ProyectoGerencia.ViewModels.RegistroNormalVMs
 {
-    public class RegistroRepresentanteLegal2VM
+    public class RegistroSegundoRepresentanteLegalVM
     {
         [Required(ErrorMessage = "Debe seleccionar un tipo de identificación")]
         [Display(Name = "Tipo de Identificación")]
@@ -18,20 +18,20 @@ namespace ProyectoGerencia.ViewModels.RegistroNormalVMs
         [Display(Name = "Identificación")]
         [MinLength(5, ErrorMessage = "La identificación debe contener más de 5 caracteres")]
         [MaxLength(30, ErrorMessage = "La identificación no puede contener más de 30 caracteres")]
-        public string Identificacion { get; set; }
+        public string Cedula { get; set; }
 
         [Required(ErrorMessage = "Ingrese el nombre completo")]
         [Display(Name = "Nombre completo")]
         [MinLength(2, ErrorMessage = "El nombre debe contener más de 2 caracteres")]
         [MaxLength(100, ErrorMessage = "El nombre no puede contener más de 100 caracteres")]
-        public string Nombre { get; set; }
+        public string NombreCompleto { get; set; }
 
         [Required(ErrorMessage = "Ingrese el correo electrónico")]
         [Display(Name = "Correo electrónico")]
         [MinLength(7, ErrorMessage = "El correo electrónico debe contener más de 7 caracteres")]
         [MaxLength(100, ErrorMessage = "El correo electrónicon no puede contener más de 100 caracteres")]
         [RegularExpression(@"^(([^<>()\[\]\\.,;:\s@""]+(\.[^<>()\[\]\\.,;:\s@""]+)*)|("".+""))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$", ErrorMessage = "El correo no es válido")]
-        public string Correo { get; set; }
+        public string CorreoElect { get; set; }
 
         public string CorreoElectronicoPersonaJuridica { get; set; }
 
