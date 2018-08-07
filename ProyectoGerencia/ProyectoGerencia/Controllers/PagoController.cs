e21318f9c96e24c627fad85b7c281efb920d968e
Ricardo Gamboa <ricardogamboa@live.com>
2018-08-07 02:31:35 -0600

    Pago PayPal agregado

---
+++ ProyectoGerencia/ProyectoGerencia/Controllers/PagoController.cs
@@ -0,0 +1,35 @@
+﻿using ProyectoGerencia.ViewModels.Pago;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoGerencia.Controllers
+{
+    public class PagoController : Controller
+    {
+        // GET: Pago
+        public ActionResult Index()
+        {
+            return View(new PagoVM {
+                Montos = new List<string>(),
+                NewMonto = 0
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Index(PagoVM Pago)
+        {
+            if (Pago.Montos == null)
+                Pago.Montos = new List<string>();
+            Pago.Montos.Add(Pago.NewMonto.ToString());
+            ViewBag.Message = "Pago hecho correctamente";
+            return View(new PagoVM
+            {
+                Montos = Pago.Montos,
+                NewMonto = 0
+            });
+        }
+    }
+}
---
+++ ProyectoGerencia/ProyectoGerencia/ViewModels/Pago/PagoVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoGerencia.ViewModels.Pago
+{
+    public class PagoVM
+    {
+        public List<string> Montos { get; set; }
+
+        [Required]
+        public int NewMonto { get; set; }
+    }
+}
